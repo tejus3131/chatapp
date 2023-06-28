8b4ecc792fda77e49cacce160be32385d4e81b79
Tejus Gupta <tejus3131@gmail.com>
2023-06-29 00:08:17 +0530

    test

--- index.py
+++ index.py
@@ -1,6 +1,34 @@
-from module import create_app
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-app = create_app()
+app = Flask(__name__)
+CORS(app)
+
+messages = []
+
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    return jsonify(messages)
+
+@app.route('/messages', methods=['POST'])
+def add_message():
+    content = request.json.get('content')
+    sender = request.json.get('sender')
+
+    if content and sender:
+        message = {
+            'content': content,
+            'sender': sender
+        }
+        messages.append(message)
+        return jsonify({'message': 'Message sent successfully'})
+    else:
+        return jsonify({'error': 'Missing content or sender'}), 400
+    
+@app.route("/clear")
+def clear_messages():
+    messages.clear()
+    return jsonify({'message': 'Messages cleared successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
